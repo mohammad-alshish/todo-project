c1480572778a4d6499db9e1e952b230a00bffd55
Mohammad majed alshish <eng.mohammed.majed@gmail.com>
2022-04-19 17:35:13 +0300

    update

--- js/app.js
+++ js/app.js
@@ -4,4 +4,4 @@ let F = "female"
 let G = prompt("Enter Your Gender.")
 let age = prompt("Enter Your Age"); if (age <= 0) {alert("incorrect age value")}
 let W = prompt("There is a welcoming messege,if Yo want to Skip type CONFIRM")
-if(W == "CONFIRM") {} else if (G == M) {alert(" Hi "+" Mr."+user+".......How are you today?")}else if (G == F) {alert(" Hi "+" Mrs."+user+".......How are you today?")}else alert(" Hi "+user+".......How are you today?"+"....By the way next time type (male or female).ok?")
+if(W == "CONFIRM") {} else if (G == M) {alert(" Hi "+" Mr."+user+".......How are you today?")}else if (G == F) {alert(" Hi "+" Mrs."+user+".......How are you today?")}else alert(" Hi "+user+".......How are you today?"+"....:):)")
