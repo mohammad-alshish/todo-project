2e08cb059336275fa574dbecf579c9897b294a6c
Mohammad majed alshish <eng.mohammed.majed@gmail.com>
2022-04-23 13:25:21 +0300

    gender empty massege problem fix

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 let user = prompt("Enter Your Name.")
 let M = "male"
 let F = "female"
-let G = prompt("Enter Your Gender.")
+let G = prompt("Enter Your Gender.");if ( G.length == 0 ){alert("Invaled Gender,Your Welcoming Messege will be without title")}
 let age = prompt("Enter Your Age"); if (age <= 0) {alert("incorrect age value")}
 let W = prompt("There is a welcoming messege,if Yo want to Skip type CONFIRM")
-if(W == "CONFIRM") {} else if (G == M) {alert(" Hi "+" Mr."+user+".......How are you today?")}else if (G == F) {alert(" Hi "+" Mrs."+user+".......How are you today?")}else alert(" Hi "+user+".......How are you today?"+"....:):)")
+if(W == "CONFIRM") {
+} else if (G == M) {
+    alert(" Hi "+" Mr."+user+".......How are you today?")
+}else if (G == F) {
+    alert(" Hi "+" Mrs."+user+".......How are you today?")
+}else alert(" Hi "+user+".......How are you today?"+"....:):)")
