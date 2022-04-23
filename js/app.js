0fe08fade2ce7bc3f7ae5069bdad136a060609a1
Mohammad majed alshish <eng.mohammed.majed@gmail.com>
2022-04-23 13:42:41 +0300

    empty gender solved

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let user = prompt("Enter Your Name.")
 let M = "male"
 let F = "female"
-let G = prompt("Enter Your Gender.");if ( G.length == 0 ){alert("Invaled Gender,Your Welcoming Messege will be without title")}
+let G = prompt("Enter Your Gender.");if ( G.length == 0 ){alert("Invaled Vlaue,Your Welcoming Messege will be without title")}
 let age = prompt("Enter Your Age"); if (age <= 0) {alert("incorrect age value")}
 let W = prompt("There is a welcoming messege,if Yo want to Skip type CONFIRM")
 if(W == "CONFIRM") {
